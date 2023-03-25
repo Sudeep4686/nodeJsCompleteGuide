25ceaacf4dfb91da427f82643d322dc57a1d2e0c
Sudeep4686 <sudeephipparge@gmail.com>
2023-03-25 11:09:43 +0530

    firstResponseToServer

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const http = require(`http`);
 
 http.createServer((req,res)=>{
-    console.log('sudeep');
-    res.write("sudeep");
-    res.end();
+    const url = req.url;
+    if (url==='/'){
+        console.log('sudeep');
+        res.write(`<html>`);
+        res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`);
+        res.write(`</html>`);
+        return res.end();
+        } 
+
+        res.setHeader("Content-Type","text/html");
+        res.write(`<html>`);
+        res.write(`<h1>welcome to my nodejs project</h1>`);
+        res.write(`</html>`)
+    
 }).listen(4000);
